7c86efb7f9262233329dbaab42654efc89719f17
Sunil Basu Pawar <68272031+SunilPawarGit@users.noreply.github.com>
2022-07-21 17:39:28 +0530

    Update users.js

--- src/api/users.js
+++ src/api/users.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://node-fcm-cafc7-default-rtdb.firebaseio.com",
+  baseURL: "https://node-fcm-cafc7-default-rtdb.firebaseio.com/",
 });
