ba7bd0c9e29b9c663abbdf110ca8be0658a59e6d
Sunil Basu Pawar <68272031+SunilPawarGit@users.noreply.github.com>
2022-07-21 15:39:43 +0530

    for analytics

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Alert from "./components/Alert";
 import Signin from "./components/Signin";
 import { Routes, Route } from "react-router-dom";
 import Success from "./components/Success";
+import reactGA from "react-ga4";
+import { useEffect } from "react";
+
+reactGA.initialize("G-R0371QD07B");
 function App() {
   const [alert, setAlert] = useState(null);
   const [type, isType] = useState("");
@@ -19,6 +23,9 @@ function App() {
   const setAuth = (uType) => {
     sessionStorage.setItem("userType", uType);
   };
+  useEffect(() => {
+    reactGA.pageview(window.location.pathname + window.location.search);
+  });
   return (
     <>
       <Alert alert={alert} />
